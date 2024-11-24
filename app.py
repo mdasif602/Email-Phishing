92b0f6add80947d8a4120df4e283dc44087921d1
Asif <mdasifhussain268@gmail.com>
2024-11-24 22:25:09 +0530

    Changes

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email Phishing Classifier")
+st.title("Email Phishing Detector")
 
-input_email = st.text_area("Enter the message")
+input_email = st.text_area("Enter the email")
 
 if st.button('Predict'):
 
